9347d8e66920b9d55395f5fe1e51ba7f252b3518
Maksim Titiaiev <gipsmas@gmail.com>
2017-02-26 02:44:53 +0200

    add less compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,46 @@
 'use strict';
-//внести свои настройки
-//добавить имя(строку, например 'projectname') проекта для тунеля,
-//который будет выглядить так - http://projectmane.localtunnel.me
+/*
+* gulp           - собрать всё, запустить вебсервер, следить за изменениями файлов
+* gulp build     - собрать всё
+* gulp clean     - удалить папку назначения
+* gulp watch     - следить за изменениями файлов
+* gulp webserver - запустить вебсервер
+* gulp deploy    - загрузить собраный проект по ftp
+* gulp html:build | sass:build | less:build | js:build | fonts:build | image:build
+*/
+/* НАСТРОЙКИ */
+/* 
+*добавить имя(строку, например 'projectname') проекта для тунеля,
+*который будет выглядить так - http://projectmane.localtunnel.me
+*/
 var projectname = '';
 
-//настройки ftp
-var hostname = 'example.com',//домен проекта, например example.com
-    username = 'login',//логин, например admin
-    userpassword = 'password',//пароль, например password
-    webpath = 'public_html';//удалённая папка, например public_html
+/* настройки ftp */
+var hostname = 'example.com', //домен проекта, например example.com
+    username = 'admin',         //логин, например admin
+    userpassword = 'pass',     //пароль, например password
+    webpath = 'public_html';         //удалённая папка, например public_html
 
-var sourceFolder = 'src',//указать папку с исходниками
-    resultFolder = 'dest';//папка с результатом (будет создана)
+var sourceFolder = 'src',       //папка с исходниками
+    resultFolder = 'dest',      //папка назначения
+    useSass = false,            //выбрать Sass или Less
+    useLess = true;
+/* КОНЕЦ НАСТРОЕК */
 
+/* для обработки выбора между sass и less */
+var sassOrLess,
+    watchStyle;
+if (useSass) {
+    watchStyle = sourceFolder + '/style/**/*.scss';
+    sassOrLess = 'sass:build';
+}
+else if (useLess) {
+    watchStyle = sourceFolder + '/style/**/*.less';
+    sassOrLess = 'less:build';
+}
+else console.log('Ошибка! Выбирете один из препроцесоров!');
+
+/* переменные с плагинами */
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
     prefixer = require('gulp-autoprefixer'),
@@ -26,8 +54,10 @@ var gulp = require('gulp'),
     rimraf = require('rimraf'),
     ftp = require('vinyl-ftp'),
     browserSync = require("browser-sync"),
+    less = require('gulp-less'),
     reload = browserSync.reload;
 
+/* объект с путями*/
 var path = {
     build: {
         html: resultFolder + '/',
@@ -39,38 +69,43 @@ var path = {
     src: {
         html: sourceFolder + '/*.html',
         js: sourceFolder + '/js/main.js',
-        style: sourceFolder + '/style/main.scss',
+        sass: sourceFolder + '/style/main.scss',
+        less: sourceFolder + '/style/main.less',
         img: sourceFolder + '/img/**/*.*',
         fonts: sourceFolder + '/fonts/**/*.*'
     },
     watch: {
         html: sourceFolder + '/**/*.html',
         js: sourceFolder + '/js/**/*.js',
-        style: sourceFolder + '/style/**/*.scss',
+        style: watchStyle,
         img: sourceFolder + '/img/**/*.*',
         fonts: sourceFolder + '/fonts/**/*.*'
     },
     clean: './'+resultFolder
 };
 
+/* конфигурация вебсервера */
 var config = {
     server: {
         baseDir: "./" + resultFolder
     },
-    tunnel: projectname || true,//Demonstration page: http://projectmane.localtunnel.me
+    tunnel: projectname || true,//Demonstration page: http://projectname.localtunnel.me
     host: 'localhost',
     port: 9000,
     logPrefix: "Frontend_Assembly"
 };
 
+/* запуск вебсервера */
 gulp.task('webserver', function () {
     browserSync(config);
 });
 
+/* удаление папки назначения */
 gulp.task('clean', function (cb) {
     rimraf(path.clean, cb);
 });
 
+/* сборка html */
 gulp.task('html:build', function () {
     gulp.src(path.src.html) 
         .pipe(rigger())
@@ -78,6 +113,7 @@ gulp.task('html:build', function () {
         .pipe(reload({stream: true}));
 });
 
+/* сборка js */
 gulp.task('js:build', function () {
     gulp.src(path.src.js) 
         .pipe(rigger()) 
@@ -88,8 +124,9 @@ gulp.task('js:build', function () {
         .pipe(reload({stream: true}));
 });
 
-gulp.task('style:build', function () {
-    gulp.src(path.src.style) 
+/* сборка sass */
+gulp.task('sass:build', function () {
+    gulp.src(path.src.sass) 
         .pipe(sourcemaps.init())
         .pipe(sass({
             includePaths: [sourceFolder + '/style/'],
@@ -104,6 +141,25 @@ gulp.task('style:build', function () {
         .pipe(reload({stream: true}));
 });
 
+/* сборка less */
+gulp.task('less:build', function () {
+    gulp.src(path.src.less)
+//        .pipe(sourcemaps.init())
+        .pipe(less({
+            includePaths: [sourceFolder + '/style/less/'],
+            outputStyle: 'compressed',
+//            sourceMap: true,
+            errLogToConsole: true
+        }))
+        .pipe(prefixer())
+//        .pipe(cssmin())
+//        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.build.css))
+        .pipe(reload({stream: true}));
+//        .pipe(gulp.dest('./public/css'));
+});
+
+/* оптимизация графики */
 gulp.task('image:build', function () {
     gulp.src(path.src.img) 
         .pipe(imagemin({
@@ -116,26 +172,28 @@ gulp.task('image:build', function () {
         .pipe(reload({stream: true}));
 });
 
+/* сборка шрифтов */
 gulp.task('fonts:build', function() {
     gulp.src(path.src.fonts)
         .pipe(gulp.dest(path.build.fonts))
 });
 
+/* сборка html, стилей, js, шрифтов, оптимизация графики */
 gulp.task('build', [
     'html:build',
     'js:build',
-    'style:build',
+    sassOrLess,
     'fonts:build',
     'image:build'
 ]);
 
-
+/* следить за изменением файлов */
 gulp.task('watch', function(){
     watch([path.watch.html], function(event, cb) {
         gulp.start('html:build');
     });
     watch([path.watch.style], function(event, cb) {
-        gulp.start('style:build');
+        gulp.start(sassOrLess);
     });
     watch([path.watch.js], function(event, cb) {
         gulp.start('js:build');
@@ -148,8 +206,7 @@ gulp.task('watch', function(){
     });
 });
 
-
-//ftp загрузка на сервер
+/* ftp загрузка на сервер */
 gulp.task('deploy', function() {
 
 	var conn = ftp.create({
@@ -170,5 +227,5 @@ gulp.task('deploy', function() {
 
 });
 
-
+/* сборка по дефолту */
 gulp.task('default', ['build', 'webserver', 'watch']);
